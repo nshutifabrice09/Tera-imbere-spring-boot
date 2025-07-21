e0065887b166a2076c1fc4e9e2a9ce732bc12b45
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-21 11:39:34 +0200

    repositories-commit

--- src/main/java/com/tsl/Tera_imbere/services/EmailDetailsServiceImplementation.java
+++ src/main/java/com/tsl/Tera_imbere/services/EmailDetailsServiceImplementation.java
@@ -1,9 +1,33 @@
 package com.tsl.Tera_imbere.services;
 import com.tsl.Tera_imbere.model.EmailDetails;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
-public class EmailDetailsServiceImplementation {
+public class EmailDetailsServiceImplementation implements EmailDetailsService{
+
+    @Override
+    public List<EmailDetails> emailDetailsList() {
+        return null;
+    }
+
+    @Override
+    public EmailDetails getEmailDetailsById(Long id) {
+        return null;
+    }
+
+    @Override
+    public EmailDetails saveEmailDetails(EmailDetails emailDetails) {
+        return null;
+    }
+
+    @Override
+    public EmailDetails updateEmailDetails(Long id, EmailDetails emailDetails) {
+        return null;
+    }
+
+    @Override
+    public void removeEmailDetails(Long id) {
+
+    }
 }
