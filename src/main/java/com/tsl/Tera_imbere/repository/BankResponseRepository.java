f06e8c262092c6db6902000526e3e918caa25fa7
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-21 11:49:02 +0200

    update-emailDetailsServiceImp-commit

--- src/main/java/com/tsl/Tera_imbere/repository/BankResponseRepository.java
+++ src/main/java/com/tsl/Tera_imbere/repository/BankResponseRepository.java
@@ -1,2 +1,9 @@
-package com.tsl.Tera_imbere.repository;public interface BankResponseRepository {
+package com.tsl.Tera_imbere.repository;
+
+import com.tsl.Tera_imbere.model.BankResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankResponseRepository extends JpaRepository <BankResponse, Long> {
 }
