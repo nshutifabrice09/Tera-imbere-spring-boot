b2c1e679285ca266ae738dfe1065ef6d72b83c43
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-21 11:30:52 +0200

    final-emailDetailsService-commit

--- src/main/java/com/tsl/Tera_imbere/model/AccountInfo.java
+++ src/main/java/com/tsl/Tera_imbere/model/AccountInfo.java
@@ -3,10 +3,8 @@ package com.tsl.Tera_imbere.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @Builder
 @Entity
 @AllArgsConstructor
