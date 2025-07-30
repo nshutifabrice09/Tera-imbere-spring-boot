a35644350780f6f451c34f95ace660b42a6ff374
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-30 10:08:15 +0200

    initial-userServiceImpl-commit

--- src/main/java/com/tsl/Tera_imbere/services/UserService.java
+++ src/main/java/com/tsl/Tera_imbere/services/UserService.java
@@ -2,8 +2,16 @@ package com.tsl.Tera_imbere.services;
 
 import com.tsl.Tera_imbere.model.BankResponse;
 import com.tsl.Tera_imbere.dto.UserRequest;
+import com.tsl.Tera_imbere.model.User;
+
+import java.util.List;
 
 public interface UserService {
 
-  
+    List<User> userList();
+    User getUserById(Long id);
+    User saveUser(User user);
+    User updateUser(Long id, User user);
+    void removeUser (Long id);
+
 }
