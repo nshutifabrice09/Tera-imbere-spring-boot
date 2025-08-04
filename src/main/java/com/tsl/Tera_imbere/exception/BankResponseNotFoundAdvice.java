e42de94772327b132d4e370e7c232de8b34b7b1a
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-04 12:27:35 +0200

    final-exceptionsAdvice-commit

--- src/main/java/com/tsl/Tera_imbere/exception/BankResponseNotFoundAdvice.java
+++ src/main/java/com/tsl/Tera_imbere/exception/BankResponseNotFoundAdvice.java
@@ -1,2 +1,24 @@
-package com.tsl.Tera_imbere.exception;public class BankResponseNotFoundAdvice {
+package com.tsl.Tera_imbere.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class BankResponseNotFoundAdvice {
+    @ResponseBody
+    @ExceptionHandler(BankResponseNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+
+    public Map<String, String> exceptionHandler(BankResponseNotFoundException exception){
+    Map<String,String> errorMap=new HashMap<>();
+    errorMap.put("errorMessage", exception.getMessage());
+    return errorMap;
+}
+
 }
