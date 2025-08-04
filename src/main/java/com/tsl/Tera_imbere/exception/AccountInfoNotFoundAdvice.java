9ea247428f0ea8d90a9a7f8deaa8f217a18ad41b
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-04 11:54:39 +0200

    exceptionsAdvice-commit

--- src/main/java/com/tsl/Tera_imbere/exception/AccountInfoNotFoundAdvice.java
+++ src/main/java/com/tsl/Tera_imbere/exception/AccountInfoNotFoundAdvice.java
@@ -1,5 +1,23 @@
 package com.tsl.Tera_imbere.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class AccountInfoNotFoundAdvice {
 
+    @ResponseBody
+    @ExceptionHandler(AccountInfoNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> exceptionHandler(AccountInfoNotFoundException exception){
+        Map<String,String> errorMap=new HashMap<>();
+        errorMap.put("errorMessage", exception.getMessage());
+        return errorMap;
+    }
 }
